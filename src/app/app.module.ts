a6ed88e66155f6588e3581e7e13d4582699b712d
Mohamed Toure <demahom.toure@gmail.com>
2024-07-05 14:48:47 +0200

    Landing page setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeaturesComponent } from './features/features.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeaturesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
